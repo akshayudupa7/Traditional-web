272f0a4ca4e002ed0ea549875e444e8aaea8a4e9
akshayudupa7 <akshayudupa7@gmail.com>
2024-01-15 20:09:52 +0530

    updating

---
+++ src/component/Boxes/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Link from "next/link";
+// Define an interface for the expected structure of the 'data' prop
+interface DataProps {
+  name: string;
+  imageUrl: string; // Corrected to lowercase 'string'
+  description: string;
+  para: string;
+  price: number;
+  help: string;
+  icon: string;
+}
+
+// Use the defined interface as the type for the 'data' prop
+export default function Index({ data }: { data: DataProps }) {
+  return (
+    <div style={{ width: "100%",marginTop:'40px' }}>
+      <Box
+        sx={{
+          backgroundColor: "#07273c",
+          boxShadow: "0 0 6px rgba(0,0,0,.8)",
+          borderRadius: "4px",
+          width: "96%",
+          height: "300px",
+          padding:"40px 0px"
+        }}
+      >
+        <Box sx={{width:"100%",display:"flex",justifyContent:"center"}}>
+        <Image src={data.icon} width={100} height={60} alt="this is image" />
+        </Box>
+      
+
+        <Typography
+          sx={{ color: "#fff", textAlign: "center", fontSize: "18px" }}
+        >
+          {data.name}
+        </Typography>
+        <Box
+          sx={{
+            borderTop: "3px solid #ff7010",
+            width: "20%",
+            margin: "auto",
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+        ></Box>
+        <Typography
+          sx={{
+            color: "#fff",
+            textAlign: "center",
+            fontSize: "14px",
+            lineHeight: "24px",
+          }}
+        >
+          {data.para}
+        </Typography>
+
+        <Link href="" style={{ textAlign: "center" }}>
+          <Typography sx={{ textAlign: "center",marginTop:"20px"}}>Read More</Typography>
+        </Link>
+      </Box>
+    </div>
+  );
+}
--- src/component/EventList/index.tsx
+++ src/component/EventList/index.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { getEvent } from "@/service/events";
+import Grid from "@mui/material/Grid";
+import Boxes from "@/component/Boxes";
+
+interface Event {
+  name: string;
+  imageUrl: string; // Corrected to lowercase 'string'
+  description: string;
+  para: string;
+  price: number;
+  help: string;
+  icon: string;
+}
+
 export default function Index() {
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+  const [val, setVal] = useState<Event[]>([]); // Specify the type as an array of Event
+
+  const getInfo = async () => {
+    try {
+      const data = await getEvent();
+      console.log(data);
+      setVal(data.message);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
   return (
     <div style={{ backgroundColor: "#031d2e", paddingTop: "80px" }}>
       <Box>
@@ -11,21 +42,37 @@ export default function Index() {
         >
           Most Popular Events
         </Typography>
-        <Box sx={{ borderTop: "3px solid #ff7010", width: "10%",margin:"auto",marginTop:"20px",marginBottom:"20px"}}>
-     
-            </Box>
+        <Box
+          sx={{
+            borderTop: "3px solid #ff7010",
+            width: "10%",
+            margin: "auto",
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+        ></Box>
         <Typography
           variant="body1"
           sx={{
             color: "#fff",
             fontSize: "16px",
             textAlign: "center",
-            boder: "2px solid #fff",
           }}
         >
           Check out the most in-demand events for your easy reference to book
           our services today!
         </Typography>
+        <Box>
+          <Grid container>
+            {val.map((item) => (
+              <Grid item xs={12} md={3}>
+                {" "}
+                {/* Add a unique key for each item */}
+                <Boxes data={item} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       </Box>
     </div>
   );
